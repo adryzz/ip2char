ffed17585aba11b192d7e0e63a1a879425a2cf7d
Lena <me@lena.place>
2023-08-24 22:49:38 +0200

    start work on serial

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ mod config;
 mod types;
 mod utils;
 
+use bytes::Bytes;
 use cidr_utils::cidr::IpCidr;
 use config::{Peer, CharPeerSection};
 use futures::{SinkExt, StreamExt};
 use packet::{builder::Builder, icmp, ip, Packet};
 use pdu::{Ethernet, EthernetPdu, Ip};
+use tokio::sync::{mpsc, broadcast};
+use tokio_serial::SerialPortBuilderExt;
 use std::{io::Read, sync::Arc};
-use tracing::{error, info};
+use tracing::{error, info, warn};
 use types::Header;
 
 use crate::config::Config;
@@ -48,8 +51,15 @@ async fn run() -> anyhow::Result<()> {
 
     let all_peers = Arc::new(config.get_all_peers());
 
+    if all_peers.len() == 0 {
+        warn!("Zero peers listed in configuration file!");
+    }
+
+    let (mspc_tx, mspc_rx) = mpsc::channel(64);
+    let (broadcast_tx, broadcast_rx) = broadcast::channel(64);
+
     for peer in all_peers.iter() {
-        tokio::task::spawn(connect_to_peer(peer.clone()));
+        tokio::task::spawn(connect_to_peer(peer.clone(), broadcast_rx.resubscribe(), mspc_tx.clone()));
     }
 
     while let Some(pkt) = framed.next().await {
@@ -76,13 +86,25 @@ async fn run() -> anyhow::Result<()> {
 }
 
 
-async fn connect_to_peer(peer: Peer) -> anyhow::Result<()> {
+async fn connect_to_peer(peer: Peer, broadcast_rx: broadcast::Receiver<Bytes>, mspc_tx: mpsc::Sender<Bytes>) {
     info!("Connecting to {}...", peer.path());
-    match peer {
-        Peer::Char(c) => connect_serial(c).await,
+    let path = peer.path().to_string();
+    let res = match peer {
+        Peer::Char(c) => connect_serial(c, broadcast_rx, mspc_tx).await,
+    };
+
+    match res {
+        Ok(_) => info!("Connection to {} closed successfully.", path),
+        Err(e) => error!("[{}] Error: {}", path, e)
     }
 }
 
-async fn connect_serial(peer: CharPeerSection) -> anyhow::Result<()> {
+async fn connect_serial(peer: CharPeerSection, mut broadcast_rx: broadcast::Receiver<Bytes>, mut mspc_tx: mpsc::Sender<Bytes>) -> anyhow::Result<()> {
+    let mut port = tokio_serial::new(&peer.path, peer.speed.unwrap_or(115200)).open_native_async()?;
+    info!("Connected to {}.", &peer.path);
+    
+    while let Ok(packet) = broadcast_rx.recv().await {
+
+    }
     Ok(())
 }
