d0fb1e40f7dff086a7a0267dc3af6fffe2e3c1c2
Lena <me@lena.place>
2023-08-26 14:34:04 +0200

    add more generic methods to handle streams

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,11 @@ use packet::{
 };
 use std::sync::Arc;
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt, Interest},
-    net::tcp::{OwnedReadHalf, OwnedWriteHalf, ReadHalf, WriteHalf},
+    io::{
+        AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, BufStream, Interest, ReadHalf,
+        WriteHalf,
+    },
+    net::tcp::{OwnedReadHalf, OwnedWriteHalf},
     select,
     sync::{broadcast, mpsc},
 };
@@ -203,15 +206,77 @@ async fn write_to_tcpstream(
     }
 }
 
+async fn read_from_stream<R>(
+    mut stream: ReadHalf<R>,
+    mpsc_tx: mpsc::Sender<Bytes>,
+) -> anyhow::Result<()>
+where
+    R: AsyncRead + Unpin,
+{
+    let mut buf = [0u8; 1500];
+    let mut header_buf = [0u8; HEADER_SIZE];
+    let mut header: Option<Header> = None;
+
+    loop {
+        if let Some(h) = header {
+            let b = &mut buf[0..h.packet_length as usize];
+            stream.read_exact(b).await?;
+            header = None;
+            mpsc_tx.send(Bytes::copy_from_slice(b)).await?;
+        } else {
+            stream.read_exact(&mut header_buf).await?;
+            header = Some(Header::from_slice(&header_buf)?);
+        }
+    }
+}
+
+async fn write_to_stream<W>(
+    mut stream: WriteHalf<W>,
+    mut broadcast_rx: broadcast::Receiver<Packet<Bytes>>,
+    peer: &Peer,
+) -> anyhow::Result<()>
+where
+    W: AsyncWrite + Unpin,
+{
+    loop {
+        let packet = broadcast_rx.recv().await?;
+        // check if packet is for us
+        if utils::check_peer_allowed_ip(&packet.destination(), peer) {
+            // generate a header
+            let mut a = Header::default();
+            a.packet_length = packet.length();
+            let header_buf: [u8; HEADER_SIZE] = a.into();
+            stream.write_all(&header_buf).await?;
+            stream.write_all(packet.as_ref()).await?;
+        }
+    }
+}
+
+async fn handle_stream<S>(
+    stream: S,
+    broadcast_rx: broadcast::Receiver<Packet<Bytes>>,
+    mpsc_tx: mpsc::Sender<Bytes>,
+    peer: Peer,
+) -> anyhow::Result<()>
+where
+    S: AsyncRead + AsyncWrite + Unpin + Send + 'static,
+{
+    let (read, write) = tokio::io::split(stream);
+    let read_task = tokio::task::spawn(read_from_stream(read, mpsc_tx));
+    write_to_stream(write, broadcast_rx, &peer).await?;
+
+    read_task.await?
+}
+
 async fn connect_serial(
     peer: CharPeerSection,
-    mut broadcast_rx: broadcast::Receiver<Packet<Bytes>>,
-    mut mspc_tx: mpsc::Sender<Bytes>,
+    broadcast_rx: broadcast::Receiver<Packet<Bytes>>,
+    mspc_tx: mpsc::Sender<Bytes>,
 ) -> anyhow::Result<()> {
-    let mut port =
+    let port =
         tokio_serial::new(&peer.path, peer.speed.unwrap_or(115200)).open_native_async()?;
     info!("Connected to {}.", &peer.path);
 
-    while let Ok(packet) = broadcast_rx.recv().await {}
+    handle_stream(port, broadcast_rx, mspc_tx, Peer::Char(peer)).await?;
     Ok(())
 }
