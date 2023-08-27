95e332f5d523c3beb7f3b52ef38dcb2e02c60c09
Lena <me@lena.place>
2023-08-27 23:36:40 +0200

    add marker in header to resync

--- src/types.rs
+++ src/types.rs
@@ -5,30 +5,41 @@ use tracing::{error, info};
 
 use crate::{utils, HEADER_SIZE};
 
-const VERSION: u16 = 0;
+pub const VERSION: u16 = 0;
+pub const MARKER_SIZE: usize = 4;
+pub const SYNC_MARKER: [u8; MARKER_SIZE] = [0xac, 0xab, 0xc0, 0xde];
 
 #[derive(Debug, Copy, Clone, Serialize, Deserialize)]
 #[repr(C)]
 pub struct Header {
+    pub marker: [u8; MARKER_SIZE],
     pub version: u16,
     pub packet_length: u16,
     pub compression: CompressionType,
     pub encryption: EncryptionType,
-    _reserved: [u8; 10],
+    _reserved: [u8; 6],
 }
 
 impl Header {
     pub fn from_slice(slice: &[u8]) -> anyhow::Result<Self> {
-        let version = *from_bytes::<u16>(&slice[..2]);
-        let packet_length = *from_bytes::<u16>(&slice[2..4]);
-        let compression = slice[4].try_into()?;
-        let encryption = slice[5].try_into()?;
+        if slice.len() < HEADER_SIZE {
+            return Err(IntoErrors::BufferTooSmall.into());
+        }
+        let marker = *from_bytes::<[u8; MARKER_SIZE]>(&slice[..4]);
+        if marker != SYNC_MARKER {
+            return Err(IntoErrors::BadSyncMarker.into());
+        }
+        let version = *from_bytes::<u16>(&slice[4..6]);
+        let packet_length = *from_bytes::<u16>(&slice[6..8]);
+        let compression = slice[8].try_into()?;
+        let encryption = slice[9].try_into()?;
         Ok(Self {
+            marker,
             version,
             packet_length,
             compression,
             encryption,
-            _reserved: [0; 10],
+            _reserved: [0; 6],
         })
     }
 }
@@ -36,11 +47,12 @@ impl Header {
 impl From<Header> for [u8; HEADER_SIZE] {
     fn from(val: Header) -> [u8; HEADER_SIZE] {
         let mut buf = [0u8; HEADER_SIZE];
-        buf[0..2].copy_from_slice(&val.version.to_le_bytes());
-        buf[2..4].copy_from_slice(&val.packet_length.to_le_bytes());
-        buf[4] = val.compression as u8;
-        buf[5] = val.encryption as u8;
-        buf[6..16].copy_from_slice(&val._reserved);
+        buf[..4].copy_from_slice(&val.marker);
+        buf[4..6].copy_from_slice(&val.version.to_le_bytes());
+        buf[6..8].copy_from_slice(&val.packet_length.to_le_bytes());
+        buf[8] = val.compression as u8;
+        buf[9] = val.encryption as u8;
+        buf[10..16].copy_from_slice(&val._reserved);
         buf
     }
 }
@@ -48,6 +60,7 @@ impl From<Header> for [u8; HEADER_SIZE] {
 impl Default for Header {
     fn default() -> Self {
         Self {
+            marker: SYNC_MARKER,
             version: VERSION,
             packet_length: 0,
             compression: Default::default(),
@@ -71,6 +84,12 @@ pub enum CompressionType {
 pub enum IntoErrors {
     #[error("no variant exists for integer {0}")]
     NoSuchVariant(u8),
+
+    #[error("Sync marker doesn't match")]
+    BadSyncMarker,
+
+    #[error("Buffer too small!")]
+    BufferTooSmall,
 }
 
 impl TryInto<CompressionType> for u8 {
